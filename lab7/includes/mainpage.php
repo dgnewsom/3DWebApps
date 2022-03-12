a8234758d8a6d70409d2a4c374821b7420168eae
Daniel Newsom <danielgnewsom@gmail.com>
2022-03-12 11:42:40 +0000

    Danny - Add Lab 7

---
+++ lab7/includes/mainpage.php
@@ -0,0 +1,77 @@
+<!-- Home Page Content -->
+<div id="home">
+    <div class="row">
+        <!-- main 3d image-->
+        <div class="col-sm-12">
+            <div id="main_3d_image">
+                <div id="main_text" >
+                    <h2 id="main_title"></h2>
+                    <h3 id="main_subtitle"></h3>
+                    <p id="main_description"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--Coca cola column-->
+        <div class="col-sm-4">
+            <div class="card">
+                <a href="javascript:showModel('coke')">
+                    <img class="card-img-top img-fluid img-thumbnail" src="assets/images/site_images/coca_cola.jpg" alt="Coca Cola">
+                </a>
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2 id="coke_title"></h2>
+                    </div>
+                    <div class="card-subtitle">
+                        <h3 id="coke_subtitle"></h3>
+                    </div>
+                    <div class="card-text">
+                        <p id="coke_description"></p>
+                    </div>
+                    <a href="http://www.coca-cola.co.uk/drinks/coca-cola/coca-cola" class="btn btn-primary" target="_blank">Find out more ...</a>
+                </div>
+            </div>
+        </div>
+        <!--Sprite column-->
+        <div class="col-sm-4">
+            <div class="card">
+                <a href="javascript:showModel('sprite')">
+                    <img class="card-img-top img-fluid img-thumbnail" src="assets/images/site_images/sprite.jpg" alt="Sprite">
+                </a>
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2 id="sprite_title"></h2>
+                    </div>
+                    <div class="card-subtitle">
+                        <h3 id="sprite_subtitle"></h3>
+                    </div>
+                    <div class="card-text">
+                        <p id="sprite_description"></p>
+                    </div>
+                    <a href="http://www.coca-cola.co.uk/drinks/sprite/sprite" class="btn btn-primary" target="_blank">Find out more ...</a>
+                </div>
+            </div>
+        </div>
+        <!--Dr Pepper column-->
+        <div class="col-sm-4">
+            <div class="card">
+                <a href="javascript:showModel('pepper')">
+                    <img class="card-img-top img-fluid img-thumbnail" src="assets/images/site_images/dr_pepper.jpg" alt="Dr Pepper">
+                </a>
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2 id="pepper_title"></h2>
+                    </div>
+                    <div class="card-subtitle">
+                        <h3 id="pepper_subtitle"></h3>
+                    </div>
+                    <div class="card-text">
+                        <p id="pepper_description"></p>
+                    </div>
+                    <a href="http://www.coca-cola.co.uk/drinks/dr-pepper/dr-pepper " class="btn btn-primary" target="_blank">Find out more ...</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
